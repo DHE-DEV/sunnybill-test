a6dcb76082ae1a589aa2b39889f1b400459c8825
Daniel Henninger <dh@passolution.de>
2025-07-08 12:52:26 +0200

    Fix NotificationsPage slug property type
    - Change slug property from string to ?string to match Filament Page interface
    - Resolve type compatibility issue with parent class

--- app/Filament/Pages/NotificationsPage.php
+++ app/Filament/Pages/NotificationsPage.php
@@ -22,7 +22,7 @@ class NotificationsPage extends Page implements HasTable, HasActions
 
     protected static ?string $navigationIcon = 'heroicon-o-bell';
     
-    protected static string $slug = 'notifications';
+    protected static ?string $slug = 'notifications';
     
     protected static ?string $title = 'Benachrichtigungen';
     
