c246011db48d424c8c3f27641eb5d5c296c3f7e7
Daniel Henninger <dh@passolution.de>
2025-07-30 20:29:15 +0200

    Entferne Statistiken-Widget von Solar Plant Billings
    - Statistiken-Component komplett entfernt
    - getHeader() Methode aus ListSolarPlantBillings entfernt
    - Originalzustand der Tabelle wiederhergestellt
    - Keine Statistiken-Funktionalität mehr vorhanden

--- app/Filament/Resources/SolarPlantBillingResource/Pages/ListSolarPlantBillings.php
+++ app/Filament/Resources/SolarPlantBillingResource/Pages/ListSolarPlantBillings.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\SolarPlantBillingResource\Pages;
 use App\Filament\Resources\SolarPlantBillingResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-use Illuminate\Contracts\View\View;
 
 class ListSolarPlantBillings extends ListRecords
 {
@@ -16,10 +15,5 @@ class ListSolarPlantBillings extends ListRecords
         return [
             // CreateAction entfernt - Abrechnungen werden über "Monatliche Abrechnungen erstellen" erstellt
         ];
-    }
-
-    public function getHeader(): ?View
-    {
-        return view('components.solar-plant-billing-statistics');
     }
 }
