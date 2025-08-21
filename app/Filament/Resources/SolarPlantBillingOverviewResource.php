e611ec1594fd0fc27f76c93930b9624d80177358
Daniel Henninger <dh@passolution.de>
2025-08-21 09:46:15 +0200

    Update SolarPlantBillingOverviewResource
    - Include any additional changes to billing overview resource
    - Ensure all modifications are committed

--- app/Filament/Resources/SolarPlantBillingOverviewResource.php
+++ app/Filament/Resources/SolarPlantBillingOverviewResource.php
@@ -29,7 +29,7 @@ class SolarPlantBillingOverviewResource extends Resource
 
     protected static ?string $navigationGroup = 'Solar Management';
 
-    protected static ?int $navigationSort = 2;
+    protected static ?int $navigationSort = 3;
 
     public static function form(Form $form): Form
     {
